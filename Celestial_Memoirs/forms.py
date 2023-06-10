dd76b9cf56a36a1d61ec0f85de7f69d17ae9e8ba
Om-Alve <omalve8174@gmail.com>
2023-06-10 21:07:56 +0530

    Bug fixes and added new background to about-us

--- Celestial_Memoirs/forms.py
+++ Celestial_Memoirs/forms.py
@@ -1,22 +1,24 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField,EmailField,PasswordField,SubmitField,BooleanField,ValidationError,DateField,TextAreaField
-from wtforms.validators import DataRequired,Length,EqualTo,Optional
+from wtforms import StringField, EmailField, PasswordField, SubmitField, BooleanField, ValidationError, DateField, TextAreaField
+from wtforms.validators import DataRequired, Length, EqualTo, Optional
 from Celestial_Memoirs.models import Users
 import datetime
 
 
 class LoginForm(FlaskForm):
-    email = EmailField('Email',validators=[DataRequired()])
-    password = PasswordField('Password',validators=[DataRequired()])
+    email = EmailField('Email', validators=[DataRequired()])
+    password = PasswordField('Password', validators=[DataRequired()])
     submit = SubmitField('Sign in')
-    remember = BooleanField('Remember me',default=True)
+    remember = BooleanField('Remember me', default=True)
+
 
 class RegisterForm(FlaskForm):
-    username = StringField('Username',validators=[DataRequired(),Length(min=2,max=20)])
-    email = EmailField('Email',validators=[DataRequired()])
-    password = PasswordField('Password',validators=[DataRequired()])
-    confirm_password = PasswordField('Confirm Password',validators=[DataRequired(),EqualTo('password')])
+    username = StringField('Username', validators=[DataRequired(), Length(min=2, max=20)])
+    email = EmailField('Email', validators=[DataRequired()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    confirm_password = PasswordField('Confirm Password', validators=[DataRequired(), EqualTo('password')])
     submit = SubmitField('Sign up')
+
     def validate_username(self, username):
         user = Users.query.filter_by(username=username.data).first()
         if user:
@@ -27,7 +29,13 @@ class RegisterForm(FlaskForm):
         if user:
             raise ValidationError('That email is taken. Please choose a different one.')
 
+
 class PostForm(FlaskForm):
     date = DateField('Date', format='%Y-%m-%d', validators=[])
     content = TextAreaField('Content', validators=[DataRequired()])
     submit = SubmitField('Add to your Space')
+
+    def validate_date(self, date):
+        live_date = datetime.date.today()
+        if date.data and date.data > live_date:
+            raise ValidationError('Invalid date. Please choose a date less than or equal to the current date.')
